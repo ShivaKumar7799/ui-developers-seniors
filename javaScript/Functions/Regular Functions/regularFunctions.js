73fb6c8798ef035c429a7ad1d8a19f0c2ec83036
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2023-06-02 13:18:38 +0530

    functions

---
+++ javaScript/Functions/Regular Functions/regularFunctions.js
@@ -0,0 +1,70 @@
+// creating a function in js
+
+// function functionName(){    
+//   // code to be executed
+//   console.log("regular function is called")
+// }
+
+//  functionName()    // invoking a function or calling a function
+
+// function sayHelloToRajaSekhar(){
+//   console.log("Hello, Good Morning Raja Sekhar")
+// }
+
+// sayHelloToRajaSekhar()
+
+
+// function sayHelloToRaji(){
+//   console.log("Hello, Good Morning Raji")
+// }
+
+// sayHelloToRaji()
+
+// function sayHelloToPrakash(){
+//   console.log("Hello, Good Morning Prakash")
+// }
+
+// sayHelloToPrakash()
+
+
+function sayHelloToPerson(personName, greetings){    // parameters - defining a function
+  console.log("Hello, Good", greetings, personName )
+}
+
+/*  sayHelloToPerson("Raja sekhar", "morning")   //  arguments  - calling a function
+sayHelloToPerson("Raji", "afternoon")
+sayHelloToPerson("Prakash", "night")
+sayHelloToPerson("venu gopal", "evening")  */
+
+
+function sendGreeting(){
+  const greetings = document.getElementById("greetings").value;
+  const personName = document.getElementById("personName").value
+  console.log("hello, Good", greetings, personName)
+  document.getElementById("showGreeting").innerHTML = "hello, Good " + greetings + " " + personName
+  // document.write("hello, Good " + greetings + " " + personName) 
+
+}
+
+// addition
+
+function add(a,b){
+  console.log( "a =", a, " b =", b, " and sum = " ,a+b);
+  return a + b
+}
+
+// add(2,3)
+// add(8,6)
+
+// add(2,5)  = 7
+
+var firstResult = add(2,5)
+
+var secondResult = add(12,27)
+
+// console.log(additionOf2and5, "addition of 2 and 5")
+// console.log(additionOf12and27, "sum of 12 and 27" )
+
+var Total = firstResult + secondResult
+
+console.log(Total, "total value")
